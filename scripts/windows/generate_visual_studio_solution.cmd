175262752a9b6b30cf0e182a74cbedb93b7f5d15
pavidloq <luberalless@yandex.ru>
2025-10-31 18:33:32 +0300

    refactor(scripts): windows build scripts
    Refactored Windows CMake build scripts.

--- scripts/windows/generate_visual_studio_solution.cmd
+++ scripts/windows/generate_visual_studio_solution.cmd
@@ -1,3 +1,15 @@
 @echo off
 
-cmake -G "Visual Studio 17 2022" -B build-visual-studio
+REM Setup directories
+set SCRIPT_DIR=%~dp0
+set SCRIPT_DIR=%SCRIPT_DIR:~0,-1%
+set REPO_ROOT=%SCRIPT_DIR%\..\..
+set BUILD_DIR=%REPO_ROOT%\build
+
+REM Remake build directory
+if exist "%BUILD_DIR%" rmdir /s /q "%BUILD_DIR%"
+mkdir "%BUILD_DIR%"
+cd /d "%BUILD_DIR%"
+
+REM Generating Visual Studio solution with CMake
+cmake "%REPO_ROOT%" -G "Visual Studio 17 2022"
